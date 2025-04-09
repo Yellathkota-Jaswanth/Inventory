82f0b834b722e15ad1344670357c87020df49216
Jaswanth Yellathkota <jaswanthyellathkota@Jaswanths-MacBook-Air.local>
2025-04-09 15:03:43 +0530

    updates

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+connectDB();
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
